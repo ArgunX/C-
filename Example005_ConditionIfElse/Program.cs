aef1e660161be98257b372535f4e3ea6e37b41f8
ArgunX <samooborona88@gmail.com>
2023-04-05 22:07:06 +0300

    Add Example005

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введте имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Привет любимчик!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
